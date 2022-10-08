f8fe46a5aa72c2da32d6a8b802c0f718e20bbd1d
Jay Giles <jay.gilesuk68@gmail.com>
2022-10-08 16:44:39 +0100

    Add project files.

---
+++ ExpressionParserLibrary/ExpressionEvaluator.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.RegularExpressions;
+using Throw;
+
+namespace ExpressionParserLibrary;
+
+public static class ExpressionEvaluator
+{
+    public static decimal? EvaluateExpression(string expression)
+    {
+        decimal calculated = new(0);
+
+        var validExpression = CleanCheckExpression(expression);
+        validExpression.ThrowIfNull("Invalid characters in expression");
+
+        var parsed = ParseExpression(validExpression);
+        parsed.ThrowIfNull("Invalid expression");
+
+        // ReSharper disable once ForCanBeConvertedToForeach
+        // ReSharper disable once LoopCanBeConvertedToQuery
+        for (var i = 0; i < parsed.Count; i++) calculated = decimal.Add(calculated, parsed[i]);
+
+        return calculated;
+    }
+
+    private static IList<decimal>? ParseExpression(string expression)
+    {
+        const string operators = @"+-*/";
+        var result = new List<decimal>();
+        var position = 0;
+        var anchor = 0;
+        bool running;
+
+        do
+        {
+            // keep going until the first operator or end of string
+            if (operators.Contains(expression[position]) || position == expression.Length - 1)
+            {
+                //check there aren't 2 operators together
+                if (position != expression.Length - 1 && operators.Contains(expression[position + 1]))
+                    return null;
+
+                //if at end of string then add 1 to the length to capture whole number
+                var eos = position == expression.Length - 1 ? 1 : 0;
+                var isConverted = decimal.TryParse(expression.AsSpan(anchor, position - anchor + eos),
+                    out var numberResult);
+
+                isConverted.Throw("Invalid conversion").IfFalse();
+
+                result.Add(numberResult);
+                anchor = position + 1;
+            }
+
+            running = position != expression.Length - 1;
+            position++;
+        } while (running);
+
+
+        return result;
+    }
+
+    private static string? CleanCheckExpression(string expression)
+    {
+        //remove all white space
+        var clean = Regex.Replace(expression, @"\s+", "");
+
+        //regex checks only integers and +-*/ operators are in the string
+        return Regex.IsMatch(clean, @"^([0-9-*+/])*$") ? clean : null;
+    }
+}
---
+++ ExpressionParserTests/ParserTests.cs
@@ -0,0 +1,35 @@
+﻿using ExpressionParserLibrary;
+
+namespace ExpressionParserTests;
+
+public class ExpressionCalculatorTests
+{
+    [Theory]
+    [InlineData("2+-1", "Invalid expression (Parameter 'parsed')")]
+    [InlineData("2+(1-1)", "Invalid characters in expression (Parameter 'validExpression')")]
+    public void Invalid_Expression_ReturnNull(string expression, string exception)
+    {
+        void Action()
+        {
+            ExpressionEvaluator.EvaluateExpression(expression);
+        }
+
+        var ex = Assert.Throws<ArgumentNullException>(Action);
+
+        Assert.Equal(exception, ex.Message);
+    }
+
+    [Theory]
+    [InlineData("2 +1", 3)]
+    [InlineData("22+11", 33)]
+    [InlineData("2- 1", 1)]
+    [InlineData(" 2 * 2 ", 4)]
+    [InlineData("4/2", 2)]
+    [InlineData("2+4/2+3*2-5", 5)]
+    public void Valid_Expression_ReturnArray(string expression, decimal expected)
+    {
+        var check = ExpressionEvaluator.EvaluateExpression(expression);
+
+        Assert.Equal(expected, check);
+    }
+}
